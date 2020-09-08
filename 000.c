9de9db8dc530643e509e595091a07e11a5fad2d8
yutako <62001710+xcc202@users.noreply.github.com>
2020-09-08 21:51:09 +0800

    Add files via upload

--- 000.c
+++ 000.c
@@ -1,5 +1,3 @@
-
-//#include "reg52.h"
 #include "intrins.h"
 #include "codetab.h"
 #include "LQ12864.h"
@@ -9,41 +7,9 @@ void main(void)
 	OLED_Init(); //OLED???
 	while(1)
 	{
-//		OLED_Fill(0xff);
-//		delay(2000);
-//		OLED_Fill(0x00); //???
-//		delay(200);
-    /*
-		OLED_P8x16Str(0,2,"I");
-		delay(2000);
+		Draw_BMP(0,0,128,8,CXK);
+		OLED_delay(1000);
 		OLED_CLS();
-		OLED_P8x16Str(0, 2, "LOVE");
-		delay(2000);
-		OLED_CLS();
-		OLED_P8x16Str(0, 2, "YOU");
-		delay(2000);
-		OLED_CLS();
-		delay(2000);
-		OLED_P8x16Str(0, 2, "I LOVE YOU !");
-		delay(1000);
-		OLED_CLS();
-		delay(1000);
-		//2
-		OLED_P8x16Str(0, 2, "I LOVE YOU !");
-		delay(1000);
-		OLED_CLS();
-		delay(1000);
-		//3
-		OLED_P8x16Str(0, 2, "I LOVE YOU !");
-		delay(1000);
-		OLED_CLS();
-		delay(3000);
-		*/
-		
-		OLED_P8x16Str(0, 2, "I LOVE YOU !");
-		delay(400);
-		OLED_CLS();
-		delay(400);
-		
+		OLED_delay(1000);
 	}
 }
